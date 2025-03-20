9028083aaf87ef1e407f9503b12d9288e8d8b7c0
Maspi <smaspons@bgeo.es>
2025-03-20 12:04:04 +0000

    feat(database): try 2 times to connect to database

--- app/database.py
+++ app/database.py
@@ -1,6 +1,7 @@
 import psycopg2
 import psycopg2.pool
 from contextlib import contextmanager
+from time import sleep
 
 from . import config
 
@@ -15,19 +16,34 @@ password = config.get_str("database", "password")
 DATABASE_URL = f"postgresql://{user}:{password}@{host}:{port}/{dbname}"
 SCHEMA_NAME = config.get_str("database", "schema")
 
-# Initialize the connection pool
-try:
-    connection_pool = psycopg2.pool.SimpleConnectionPool(
-        1,  # Minimum number of connections
-        10, # Maximum number of connections
-        DATABASE_URL
-    )
-except Exception as e:
-    print(e)
-    connection_pool = None
+connection_pool = None
+
+def init_conn_pool():
+    global connection_pool
+    # Initialize the connection pool
+    try:
+        connection_pool = psycopg2.pool.SimpleConnectionPool(
+            1,  # Minimum number of connections
+            10, # Maximum number of connections
+            DATABASE_URL
+        )
+    except Exception as e:
+        print(e)
+        connection_pool = None
+
+init_conn_pool()
 
 @contextmanager
 def get_db():
+    global connection_pool
+    max_tries = 2
+    n_try = 0
+    while connection_pool is None and n_try < max_tries:
+        init_conn_pool()
+        n_try += 1
+        if connection_pool is None:
+            sleep(5)
+
     if connection_pool is None:
         yield None
         return
